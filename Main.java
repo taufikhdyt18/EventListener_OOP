00b86d7d857be67317ec53854bf2b1de97c57183
TaufikHidayat <148044530+taufikhdyt18@users.noreply.github.com>
2025-01-03 14:29:15 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,30 @@
 import classes.Database;
 import controller.MahasiswaController;
+import controller.NilaiController;
 import java.sql.Connection;
 import model.MahasiswaModel;
+import model.NilaiModel;
 import view.FormMahasiswa;
+import view.FormNilai;
 
 public class Main {
+    private static MahasiswaController mahasiswaController;
+    private static NilaiController nilaiController;
+
     public static void main(String[] args) {
         try {
             Connection connection = Database.getConnection();
-            MahasiswaModel model = new MahasiswaModel(connection);
-            FormMahasiswa view = new FormMahasiswa();
-            MahasiswaController controller = new MahasiswaController(model, view);
+            MahasiswaModel mahasiswaModel = new MahasiswaModel(connection);
+            NilaiModel nilaiModel = new NilaiModel(connection);
+            FormMahasiswa formMahasiswa = new FormMahasiswa();
+            FormNilai formNilai = new FormNilai();
+
+            mahasiswaController = new MahasiswaController(mahasiswaModel, formMahasiswa);
+            nilaiController = new NilaiController(nilaiModel, mahasiswaModel, formNilai);
+
             
-            view.setVisible(true);
+            formNilai.setVisible(true); 
+            
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
